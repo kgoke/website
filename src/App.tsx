0729233ec9934dd44f2e928c4235c16765476e8c
kgoke <kg333920@ohio.edu>
2023-12-26 13:35:51 -0500

    added stuff

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,15 @@ function App() {
             <img src={Clouds} className="w-full h-full object-cover object-center absolute z-10 animate-pulse"/>
             <img src={Astronaut} className="absolute z-0 animate-bounce left-1/3 top-1/4"/>
             <div className="flex flex-col w-full h-full justify-center items-center absolute z-20 bg-black bg-opacity-70">
-                <h1 className='text-3xl font-bold text-pink-300'>KGOKE.COM</h1>
                 <ul className='flex flex-col justify-center items-center'>
                     <li>
-                        <a className='text-2xl text-pink-300 hover:font-bold' href='https://www.github.com/kgoke' target='_blank'>Github</a>
+                        <a className='text-4xl text-pink-300 hover:font-bold' href='https://www.github.com/kgoke' target='_blank'>Github</a>
                     </li>
                     <li>
-                        <a className='text-2xl text-pink-300 hover:font-bold' href='https://www.linkedin.com/in/keegan-goecke-97638821b' target='_blank'>LinkedIn</a>.
+                        <a className='text-4xl text-pink-300 hover:font-bold' href='https://www.linkedin.com/in/keegan-goecke-97638821b' target='_blank'>LinkedIn</a>.
                     </li>
                     <li>
-                        <a className='text-2xl text-pink-300 hover:font-bold' href='https://www.youtube.com/@kgoke' target='_blank'>YouTube</a>.
+                        <a className='text-4xl text-pink-300 hover:font-bold' href='https://www.youtube.com/@kgoke' target='_blank'>YouTube</a>.
                     </li>
 
                 </ul>
